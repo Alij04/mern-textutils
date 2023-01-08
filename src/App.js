c3f55af534400cfc5e9166e594213fb002af39b1
Alij04 <bsef19m543@pucit.edu.pk>
2023-01-09 01:10:41 +0500

    9 jan 2023

--- src/App.js
+++ src/App.js
@@ -79,7 +79,7 @@ function App() {
   const lightMode = () => {
     setMode("light");
     document.body.style.backgroundColor = "white";
-    showAlert("Light mode has been enabled", "success");
+    showAlert(" mode has been enabled", "success");
   };
 
   return (
